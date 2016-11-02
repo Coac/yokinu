1c1e853340fe804e23426153f8858e9f5198a181
Coac <levictor3000@hotmail.fr>
2016-11-02 10:40:24 +0100

    Added semistandard eslint

--- config/index.js
+++ config/index.js
@@ -3,13 +3,13 @@
 var config;
 
 try {
-    config = require('./config.json');
+  config = require('./config.json');
 } catch (e) {
-    try {
-        config = require('./config.default.json');
-    } catch (e) {
-        throw new Error('No config file found.');
-    }
+  try {
+    config = require('./config.default.json');
+  } catch (e) {
+    throw new Error('No config file found.');
+  }
 }
 
 module.exports = config;
--- modules/gmusic/index.js
+++ modules/gmusic/index.js
@@ -4,15 +4,14 @@ const config = require('../../config').gmusic;
 const router = require('express').Router();
 const Library = require('./library');
 
-
 if (!config) throw new Error('No config found for module : gmusic');
 
 router.get('/', (req, res, next) => {
-    var library = new Library();
-    library
+  var library = new Library();
+  library
         .init({
-            email: config.email,
-            password: config.password
+          email: config.email,
+          password: config.password
         })
         .then(res.json.bind(res))
         .catch(next);
